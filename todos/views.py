7be53b164bebbdc511ea13d64b4583554bec1cf6
kweku-xvi <nksarps@gmail.com>
2023-11-02 21:25:31 +0000

    modifying views for CRUD functions

--- todos/views.py
+++ todos/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from .serializers import TodoSerializer
-from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.decorators import api_view, permission_classes
 from .models import Todo
 from rest_framework import response, status
 from rest_framework.permissions import IsAuthenticated
 
 
-#create todo
-@api_view(['POST'])
+# create and list todos 
+@api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
-def create_todo(request):
-    if request.method == 'POST':
+def create_and_list_todos(request):
+    if request.method == 'POST': # creating a todo
         serializer = TodoSerializer(data=request.data)
 
         if serializer.is_valid():
@@ -19,45 +19,28 @@ def create_todo(request):
             return response.Response(serializer.data, status=status.HTTP_201_CREATED)
         return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-
-#list all todos
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def list_todos(request):
-    if request.method == 'GET':
+    elif request.method == 'GET': # retrieving all todos created by the current user
         todos = Todo.objects.filter(owner=request.user)
         serializer = TodoSerializer(todos, many=True)
 
         return response.Response(serializer.data, status=status.HTTP_200_OK)
 
 
-
-# list one todo
-@api_view(['GET'])
+# reading, updating and deleting todos
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
 @permission_classes([IsAuthenticated])
-def get_todo(request, pk):
-    if request.method == 'GET':
-        try:
-            todo = Todo.objects.filter(owner=request.user).get(id=pk)
-        except Todo.DoesNotExist:
-            return response.Response({'message':'ID specified does not except'},status=status.HTTP_404_NOT_FOUND)
+def todo_details(request, pk):
+    try:
+        todo = Todo.objects.filter(owner=request.user).get(id=pk)
+    except Todo.DoesNotExist:
+        return response.Response({'message':'Object does not exist'}, status=status.HTTP_404_NOT_FOUND)
 
+    if request.method == 'GET': # retrieving an object
         serializer = TodoSerializer(todo)
 
         return response.Response(serializer.data, status=status.HTTP_200_OK)
 
-
-# update one todo 
-@api_view(['PUT', 'PATCH'])
-@permission_classes([IsAuthenticated])
-def update_todo(request, pk):
-    if request.method == 'PUT' or request.method == 'PATCH':
-        try:
-            todo = Todo.objects.filter(owner=request.user).get(id=pk)
-        except Todo.DoesNotExist:
-            return response.Response({'message':'Todo object specified does not exist'}, status=status.HTTP_404_NOT_FOUND)
-
+    elif request.method == 'PUT' or request.method == 'PATCH': # updating an object
         serializer = TodoSerializer(todo, data=request.data, partial=True)
 
         if serializer.is_valid():
@@ -66,17 +49,7 @@ def update_todo(request, pk):
             return response.Response(serializer.data, status=status.HTTP_200_OK)
         return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-# delete one todo 
-@api_view(['DELETE'])
-@permission_classes([IsAuthenticated])
-def delete_todo(request, pk):
-    try:
-            todo = Todo.objects.filter(owner=request.user).get(id=pk)
-    except Todo.DoesNotExist:
-        return response.Response({'message':'Todo object specified does not exist'})
-
-    if request.method == 'DELETE':
+    elif request.method == 'DELETE': # deleting an item
         todo.delete()
 
         return response.Response({'message':'Object has been deleted'}, status=status.HTTP_204_NO_CONTENT)
