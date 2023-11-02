8f276b1ebae02383a295c679b66d5f310b8cf2f2
kweku-xvi <nksarps@gmail.com>
2023-11-02 21:26:06 +0000

    added url endpoints for views.py

--- todos/urls.py
+++ todos/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create/', views.create_todo, name='create'),
-    path('list/', views.list_todos, name='list-todos'),
-    path('<int:pk>/', views.get_todo, name='get-one-todo'),
-    path('<int:pk>/update/', views.update_todo, name='update-todo'),
-    path('<int:pk>/delete/', views.delete_todo, name='delete-todo'),
+    path('', views.create_and_list_todos, name='todos'),
+    path('<int:pk>/', views.todo_details, name='todo-detail'),
 ]
